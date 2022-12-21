e1f90e63e01bad3c1891e7dc0818d8d9677b79dc
Caleb <benJephunneh@users.noreply.github.com>
2022-12-21 17:01:17 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -71,7 +71,8 @@ export default class MyApp extends App {
             content="width=device-width, initial-scale=1, shrink-to-fit=no"
           />
           <title>Notus NextJS by Creative Tim</title>
-          <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+          {/* Next complained of the following: */}
+          {/* <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script> */}
         </Head>
         <Layout>
           <Component {...pageProps} />
